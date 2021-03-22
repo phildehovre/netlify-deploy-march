ec76f5580a3dbbdd9e99f66c1db1cbd12d557c3b
phildehvre <ph.dehovre@gmail.com>
2021-03-22 12:15:11 +0100

    axios debug sesh

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 import RandomList from './RandomList';
 import AnotherComponent from './AnotherComponent';
@@ -6,12 +7,40 @@ import './stylesheet.css'
 
 const App = () => {
 
-  return (
+  const [response, setResponse] = useState([])
+
+  useEffect(() => {
+    posts()
+  }, [])
+
+  const posts = async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/todos/')
+   setResponse(res.data)
+  }
+    
+    
+    const renderPosts = () => {
+      return response.map(post => {
+        return (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+
+          </div>
+        )
+      })
+    }
+    
+    return (
     <div>
       Application
+
+      Axios Test 1
+
       <RandomList />  
       <AnotherComponent />
       <AnotherComponent />
+      {renderPosts()}
     </div>
   )
 }
